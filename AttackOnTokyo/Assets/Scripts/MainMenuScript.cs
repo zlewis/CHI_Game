f4bf39ad3535cd99f74e697ac90a03615f05d8fb
zlewis <jedimasterzl@yahoo.com>
2014-10-29 19:05:02 -0500

    Added some things
    I realized after I made the dice that this game doesn't use regular
    dice....

---
+++ AttackOnTokyo/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI() {
+	
+		GUI.Label(new Rect(Screen.width/2-50,100, 100, 20), "Main Menu");
+
+		if (GUI.Button(new Rect(Screen.width/2-(75/2),Screen.height/2-25, 75, 50), "button 1"))
+		{
+            print("1");
+		}
+	}
+}
