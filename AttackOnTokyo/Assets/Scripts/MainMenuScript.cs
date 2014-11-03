6d615b5dff45d575c23f020fb664dfd1b0c346de
Frank Tian <franktian41@gmail.com>
2014-11-02 18:54:17 -0600

    Main menu, game settings, game board

--- AttackOnTokyo/Assets/Scripts/MainMenuScript.cs
+++ AttackOnTokyo/Assets/Scripts/MainMenuScript.cs
@@ -14,12 +14,28 @@ public class MainMenuScript : MonoBehaviour {
 	}
 	
 	void OnGUI() {
-	
-		GUI.Label(new Rect(Screen.width/2-50,100, 100, 20), "Main Menu");
 
-		if (GUI.Button(new Rect(Screen.width/2-(75/2),Screen.height/2-25, 75, 50), "button 1"))
+        GUIStyle myStyle = new GUIStyle();
+        myStyle.fontSize = 50;
+        myStyle.normal.textColor = Color.white;
+
+        GUI.Label(new Rect(Screen.width / 2 - 150, 100, 300, 20), "King of Tokyo", myStyle);
+
+		if (GUI.Button(new Rect(Screen.width/2-75,2*Screen.height/5, 150, 50), "Start Game"))
 		{
-            print("1");
+            Application.LoadLevel(1);
 		}
+        if (GUI.Button(new Rect(Screen.width/2-75, Screen.height/2, 150, 50), "Game Settings"))
+        {
+            Application.LoadLevel(2);
+        }
+        if (GUI.Button(new Rect(Screen.width/2-75, 3*Screen.height/5, 150, 50), "How to play?"))
+        {
+            print("3");
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 - 75, 7 * Screen.height / 10, 150, 50), "Quit"))
+        {
+            Application.Quit();
+        }
 	}
 }
