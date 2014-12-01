263fd13177c8e0928465d583bca03bbade45f1d3
Roel Orduno <rro1020@gmail.com>
2014-12-01 15:17:48 -0800

    Added Monster Script
    Monster.cs is derived from MonsterScript.cs which just keeps track of
    the monster's stats

---
+++ AttackOnTokyo/Assets/Scripts/Monster.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonsterScript {
+
+	public int xPosition; 
+	public int yPosition; 
+
+	void OnGUI(){
+		GUI.Label (new Rect (xPosition, yPosition, 100, 100), "Health: " + "\t" + health);
+		GUI.Label (new Rect (xPosition, yPosition + 15, 100, 100), "Energy: " + energy); 
+		GUI.Label (new Rect (xPosition, yPosition + 30, 100, 100), "Points: " + "\t" + victoryPoints); 
+	}
+}
---
+++ AttackOnTokyo/Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterScript : MonoBehaviour {
+
+	public int victoryPoints; 
+	public int energy; 
+	public int health; 
+
+	private bool isAlive; 
+
+	// Use this for initialization
+	void Start () {
+		victoryPoints = 0; 
+		energy = 0; 
+		health = 10; 
+		isAlive = true; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//guiText.text = "Test " + health; 
+	}
+
+	//Methods for victoryPoints 
+	void addPoints(int num){
+		victoryPoints += num; 
+		//wins if points >= 20 
+	}
+
+	void losePoints(int num){
+		victoryPoints -= num; 
+	}
+
+	//Methods for energy 
+	void addEnergy(int num){
+		energy += num; 
+	}
+
+	void loseEnergy(int num){
+		energy -= num; 
+	}
+
+	//Methods for health 
+	void addHealth(int num){
+		if (health < 10){ 
+			health += num;
+		}
+	}
+
+	void loseHealth(int num){
+		health -= num; 
+		if (health <= 0) {
+			isAlive = false; 
+		}
+	}
+
+	//Method for isAlive
+	bool monsterAlive(){
+		return isAlive; 
+	}
+}
